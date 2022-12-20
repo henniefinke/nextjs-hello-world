1c7bbfcec47585f4f5cbd27016375bd1a75201db
henniefinke <121129666+henniefinke@users.noreply.github.com>
2022-12-20 20:48:48 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 import Link from 'next/link'
 const Index = () => (
+  <!-- Leadinfo tracking code -->
+<script>
+(function(l,e,a,d,i,n,f,o){if(!l[i]){l.GlobalLeadinfoNamespace=l.GlobalLeadinfoNamespace||[];
+l.GlobalLeadinfoNamespace.push(i);l[i]=function(){(l[i].q=l[i].q||[]).push(arguments)};l[i].t=l[i].t||n;
+l[i].q=l[i].q||[];o=e.createElement(a);f=e.getElementsByTagName(a)[0];o.async=1;o.src=d;f.parentNode.insertBefore(o,f);}
+}(window,document,"script","https://cdn.leadinfo.net/ping.js","leadinfo","LI-63A20E8FCDD4C"));
+</script>
   <div>
     Hello World.{' '}
     <Link href="/about">
